1728ccc136058d6517c83bee9f4b30808211be19
Todkhuu <todkhuu@Todkhuus-MacBook-Pro.local>
2025-11-08 14:44:38 +0800

    first

---
+++ lib/providers/cart_provider.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_riverpods/models/product.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'cart_provider.g.dart';
+
+@riverpod
+class CartNotifier extends _$CartNotifier {
+  // initial value
+  @override
+  Set<Product> build() {
+    return const {};
+  }
+
+  // methods to update state
+  void addProduct(Product product) {
+    if (!state.contains(product)) {
+      state = {...state, product};
+    }
+  }
+
+  void removeProduct(Product product) {
+    if (state.contains(product)) {
+      state = state.where((p) => p.id != product.id).toSet();
+    }
+  }
+}
+
+@riverpod
+int cartTotal(ref) {
+  final cartProducts = ref.watch(cartNotifierProvider);
+
+  int total = 0;
+
+  for (Product product in cartProducts) {
+    total += product.price;
+  }
+
+  return total;
+}
